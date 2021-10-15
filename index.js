6c249519003db9661c059b31e41cd3d34679df14
rajaditya28 <rajadityapqrst@gmail.com>
2021-10-15 08:47:38 +0530

    Added a Play Button to replace the initial reload method to play the game

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var val1 = Math.floor(Math.random()*6)+1;
-var val2 = Math.floor(Math.random()*6)+1;
-var randomdice1="dice"+val1+".png"
-var randomdice2="dice"+val2+".png"
-document.getElementsByTagName("img")[0].setAttribute("src", randomdice1);
-document.getElementsByTagName("img")[1].setAttribute("src", randomdice2);
-if(val1>val2)
-{
-    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins";
-}
-else if(val1<val2)
-{
-    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins";
-}
-else
-{
-    document.getElementsByTagName("h1")[0].innerHTML="Tie";
-}
+function refreshPage(){
+    
+    var val1 = Math.floor(Math.random()*6)+1;
+    var val2 = Math.floor(Math.random()*6)+1;
+    var randomdice1="dice"+val1+".png"
+    var randomdice2="dice"+val2+".png"
+    document.getElementsByTagName("img")[0].setAttribute("src", randomdice1);
+    document.getElementsByTagName("img")[1].setAttribute("src", randomdice2);
+    if(val1>val2)
+    {
+        document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins";
+    }
+    else if(val1<val2)
+    {
+        document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins";
+    }
+    else
+    {
+        document.getElementsByTagName("h1")[0].innerHTML="Tie";
+    }
+} 
+
